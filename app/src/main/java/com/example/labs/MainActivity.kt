1a32f4676926223ccfc4591bf3adcae358bc8e48
vladuslavbakumenko <vladgts32@gmail.com>
2023-10-09 13:52:31 +0300

    create lab 1

--- app/src/main/java/com/example/labs/MainActivity.kt
+++ app/src/main/java/com/example/labs/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.labs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.content.res.AppCompatResources
+import com.example.labs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        binding.changeTextButton.setOnClickListener {
+            binding.tv.text = "Text changed"
+            binding.root.background =  AppCompatResources.getDrawable(this, R.color.red)
+        }
     }
 }
